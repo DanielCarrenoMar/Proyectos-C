26e1310a1f1b18161c4b3358c61d5c3e09ff2f9d
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-02-04 18:44:45 -0400

    refactor: deffine speed

--- MouseSensibilidad.c
+++ MouseSensibilidad.c
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 
+#define SPEED 20
+
 int main() {
     int speed;
 
@@ -9,7 +11,7 @@ int main() {
     printf("La velocidad actual del mouse es %d\n", speed);
 
     // Cambiar la velocidad del mouse a un valor entre 1 y 20
-    int new_speed = 11;  // Cambia este valor para ajustar la sensibilidad
+    int new_speed = SPEED;  // Cambia este valor para ajustar la sensibilidad
     SystemParametersInfo(SPI_SETMOUSESPEED, 0, (void*)(uintptr_t)new_speed, 0);
     printf("La nueva velocidad del mouse es %d\n", new_speed);
 
