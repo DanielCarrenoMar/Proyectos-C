e8410d6f7300f1f4fa75fb8498def71ade2d84ac
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-03-09 19:36:37 -0400

    Feat: Tabla y cuadrado

--- Librerias/Graficos.c
+++ Librerias/Graficos.c
@@ -58,6 +58,9 @@ void gotoxy(int x, int y){
 void borrarPantalla(){
     system(CLEAR);
 }
+/*
+    Usando dos coordenadas dibuja una linea en la pantalla
+*/
 void line(int x1, int y1, int x2, int y2, char sprite){
     int paso;
 
@@ -84,15 +87,18 @@ void line(int x1, int y1, int x2, int y2, char sprite){
         }
     }
 }
-
+/*
+    Usando una serie de coordenadas dibuja un poligono
+*/
 void poligono(int  coords[][2], int elemt, char sprite){
     for (int i = 0; i < elemt-1; i++){
         line(coords[i][0], coords[i][1], coords[i + 1][0], coords[i + 1][1], sprite);
     }
 }
-
+/*
+    Dibuja un rectangulo en la pantalla
+*/
 void recuadro(int x, int y, int w, int h){
-
     for (int i = x; i < x + w; i++){
         gotoxy(i, y);
         printf("%c", 205);
@@ -110,4 +116,85 @@ void recuadro(int x, int y, int w, int h){
     gotoxy(x, y + h); printf("%c", 200);
     gotoxy(x + w, y + h); printf("%c", 188);
 }
+/*
+    Dibuja unta tabla con dimensiones especificas
+*/
+void tabla(int X , int Y, int colum, int filas, int Xsize, int Ysize){
+    int upL = 218; // ┌
+    int upR = 191; // ┐
+    int downR = 217; // ┘
+    int downL = 192; // └
+    int cross = 197; // ┼
+    int crossD = 194; // ┬
+    int crossU = 193; // ┴
+    int crossR = 195; // ├
+    int crossL = 180; // ┤
+    int vLine = 179; // │
+    int hLine = 196; // ─
 
+    gotoxy(X,Y); printf("%c", upL);
+
+    for (int c = 0; c < colum; c++){
+        for (int x = 0; x < Xsize; x++){
+            printf("%c", hLine);
+        }
+        if (c != colum - 1){
+            printf("%c", crossD);
+        }else{
+            printf("%c", upR);
+        }
+    }
+
+    for (int f = 1; f < filas; f++){
+        gotoxy(X,Y + (Ysize+1) * f); printf("%c", crossR);
+
+        for (int c = 0; c < colum; c++){
+            for (int x = 0; x < Xsize; x++){
+                printf("%c", hLine);
+            }
+            if (c != colum - 1){
+                printf("%c", cross);
+            }else{
+                printf("%c", crossL);
+            }
+        }
+    }
+
+    gotoxy(X,Y + filas * (Ysize+1)); printf("%c", downL);
+
+    for (int c = 0; c < colum; c++){
+        for (int x = 0; x < Xsize; x++){
+            printf("%c", hLine);
+        }
+        if (c != colum - 1){
+            printf("%c", crossU);
+        }else{
+            printf("%c", downR);
+        }
+    }
+
+    for (int f = 0; f < filas; f++){
+        for (int yz = 1; yz < Ysize+1; yz++){
+            gotoxy(X,Y + (Ysize+1) * f + yz); printf("%c", vLine);
+
+            for (int c = 0; c < colum; c++){
+                for (int xz = 0; xz < Xsize; xz++){
+                    printf(" ");
+                }
+                printf("%c", vLine);
+            }
+        }
+    }
+}
+/*
+    Dibuja un cuadrado relleno en la pantalla
+*/
+void cuadrado(int X, int Y, int w, int h, char sprite){
+    for (int y = Y; y < Y + h; y++){
+        gotoxy(X, y); 
+        
+        for (int x = X; x < X + w; x++){
+            printf("%c", sprite);
+        }
+    }
+}
--- Librerias/Graficos.h
+++ Librerias/Graficos.h
@@ -17,6 +17,8 @@ void desactivarmax();
 void line(int x1, int y1, int x2, int y2, char sprite);
 void poligono(int  coords[16][2], int elemt, char sprite);
 void recuadro(int x, int y, int w, int h);
+void tabla(int x , int y, int filas, int columnas, int filasize, int columnasize);
+void cuadrado(int x, int y, int w, int h, char sprite);
 
 #include "Graficos.c"
 #endif
